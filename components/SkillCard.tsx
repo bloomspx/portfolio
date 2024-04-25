59bef86d0ee7a57b181bd95807ff937a5a307eec
bloomspx <51470169+bloomspx@users.noreply.github.com>
2024-04-26 02:01:50 +0800

    fix: transitions

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -10,14 +10,6 @@ type Props = {
 function SkillCard({ directionLeft, name }: Props) {
   return (
     <div className='flex flex-col items-center gap-4 cursor-pointer'>
-      <motion.div
-        initial={{ 
-          x: directionLeft ? -200 : 200,
-          opacity: 0,
-        }}
-        transition={{ duration: 1}}
-        whileInView={{ opacity: 1, x: 0}}
-        viewport={{ once: true }}>
         <Image
           className='skillIcon'
           alt="icon"
@@ -25,7 +17,6 @@ function SkillCard({ directionLeft, name }: Props) {
           height={72}
           src={`/static/images/${name}.svg`}
         />
-      </motion.div>
 
       <div className={`flex justify-center py-1 px-3 rounded-md
         font-semibold text-shadow shadow-md shadow-gray-800
